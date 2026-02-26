b79542741d0fb97c1c83a64a7d0a54cd1e958f1d
shankar117512 <shankar117512@gmail.com>
2026-02-26 12:58:12 +0000

    Initial commit

---
+++ core/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["your-production-domain.up.railway.app"]
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
