aa1d349ad9aaacb584582285c37b8790d028cef7
shankar117512 <shankar117512@gmail.com>
2026-02-26 14:20:44 +0000

    Initial commit

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -2,3 +2,14 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dev_db",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,4 +1,9 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ["your-staging-domain.up.railway.app"]
+
+DATABASES = {
+    "default": dj_database_url.config()
+}
