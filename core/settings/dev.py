706f09bcaa5ed91082ce313f745ee193546148d5
shankar117512 <shankar117512@gmail.com>
2026-02-27 12:09:57 +0000

    CSRF added

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["https://talented-charm-dev.up.railway.app", "talented-charm-dev.up.railway.app"]
+ALLOWED_HOSTS = ["talented-charm-dev.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://talented-charm-dev.up.railway.app"]
