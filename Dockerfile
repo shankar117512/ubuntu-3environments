82ffd9a784117c7b57d6743ef698c99b6c372d28
shankar117512 <shankar117512@gmail.com>
2026-02-27 11:22:03 +0000

    fix staticfiles config

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN python manage.py collectstatic --noinput
+
 COPY . .
 
 CMD ["gunicorn", "core.wsgi:application", "--bind", "0.0.0.0:8000"]
--- core/settings/base.py
+++ core/settings/base.py
@@ -116,6 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
