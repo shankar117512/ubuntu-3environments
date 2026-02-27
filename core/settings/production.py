bd7cf410cd7ccde0b2361ffb6a37c5f8b857f6b7
shankar117512 <shankar117512@gmail.com>
2026-02-27 11:38:06 +0000

    CSRF added

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ["your-production-domain.up.railway.app"]
+ALLOWED_HOSTS = ["ubuntu-3environments-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://ubuntu-3environments-production.up.railway.app"]
 
 DATABASES = {
     "default": dj_database_url.config()
